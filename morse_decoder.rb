ccbe98446873b6a6721c5bf5fe9584d3582dbe63
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-09-21 17:25:04 +0200

    FIx linters

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -1,4 +1,4 @@
-def decode_char str
+def decode_char (str)
   morse_dict = {
     'a' => '.-',
     'b' => '-...',
@@ -40,24 +40,26 @@ def decode_char str
   morse_dict.each_key do |key|
     return key.upcase if str == morse_dict[key]
   end
-  return "unknown"
 end
 
-def decode_word str
-  str.split(" ").each do |char|
+def decode_word (str)
+  word = ''
+  str.split(' ').each do |char|
   word += decode_char(char)
   end
   return word
 end
 
-def decode str
+def decode (str)
+  message = ''
   str.split("   ").each_with_index do |word, index|
-    if index > 0 
-    message += " #{decode_word(word)}"
+    if index != 0
+      message += " #{decode_word(word)}"
     else
       message += decode_word(word)
     end
   end
   p message
-  return message
 end
+
+decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
